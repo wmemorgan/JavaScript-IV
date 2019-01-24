b94528425aadc10c0be5f05cd14764c691a66fbb
Wilfred Morgan <wil.me.morgan@gmail.com>
2019-01-24 17:54:24 +0800

    Create school personnel

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,4 +1,5 @@
 // CODE here for your Lambda Classes
+//==== Person
 class Person {
   constructor(attributes) {
     this.name = attributes.name
@@ -11,6 +12,21 @@ class Person {
   }
 }
 
+//Examples
+const mike = new Person({
+  name: 'Mike',
+  age: 38,
+  location: 'Chicago',
+  gender: 'male'
+})
+
+const carol = new Person({
+  name: 'Carol',
+  age: 37,
+  location: 'Cleveland'
+})
+
+//==== Instructor
 class Instructor extends Person {
   constructor(attributes) {
     super(attributes)
@@ -28,6 +44,26 @@ class Instructor extends Person {
   }
 }
 
+// EXAMPLES
+const alice = new Instructor({
+  name: 'Alice',
+  age: 42,
+  location:  'Kansas City',
+  specialty: 'nodeJS',
+  favLanguage: 'JavaScript',
+  catchPhrase: "Dinner's ready, come and get it!"
+})
+
+const sam = new Instructor({
+  name: 'Sam',
+  age: 45,
+  location:  'Tulsa',
+  specialty: 'pandas',
+  favLanguage: 'Python',
+  catchPhrase: "I've got nothing, bro."
+})
+
+//==== Student
 class Student extends Person {
   constructor(attributes) {
     super(attributes)
@@ -51,6 +87,44 @@ class Student extends Person {
 
 }
 
+// EXAMPLES
+const peter = new Student({
+  name: 'Peter',
+  age: 24,
+  location:  'New York City',
+  previousBackground: 'Stand-up Comic',
+  className: 'webpt4',
+  favSubjects: ['HTML', 'CSS', 'JavaScript']
+})
+
+const jan = new Student({
+  name: 'Jan',
+  age: 23,
+  location: 'Seattle',
+  previousBackground: 'Librarian',
+  className: 'cs132',
+  favSubjects: ['Python', 'C++', 'Java'] 
+})
+
+const bobby = new Student({
+  name: 'Bobby',
+  age: 20,
+  location:  'Houston',
+  previousBackground: 'Hotel Concierge',
+  className: 'ux10',
+  favSubjects: ['Ideation', 'Sketch']
+})
+
+const cindy = new Student({
+  name: 'Cindy',
+  age: 19,
+  location:  'Las Vegas',
+  previousBackground: 'Pharmaceutical Sales Rep',
+  className: 'ds30',
+  favSubjects: ['Python', 'SQL']
+})
+
+//==== Project Manager
 class ProjectManager extends Instructor {
   constructor(attributes) {
     super(attributes)
@@ -67,3 +141,26 @@ class ProjectManager extends Instructor {
   }
 
 }
+
+// EXAMPLES
+const greg = new ProjectManager({
+  name: 'Greg',
+  age: 31,
+  location:  'Ft. Lauderdale',
+  specialty: 'data structures',
+  favLanguage: 'C++',
+  catchPhrase: "Now Dad were talking Man to man to Man not kid to man but man to man to man",
+  gradClassName: 'cs50',
+  favInstructor: 'Alice'
+})
+
+const marcia = new ProjectManager({
+  name: 'Marcia',
+  age: 30,
+  location:  'Malibu',
+  specialty: 'd3',
+  favLanguage: 'SQL',
+  catchPhrase: "Oh, my nose!",
+  gradClassName: 'ds5',
+  favInstructor: 'Alice'
+})
